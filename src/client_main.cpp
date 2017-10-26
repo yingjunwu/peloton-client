e1fdd1f08c7c9257e730679fc5b22ab831be0616
Yingjun Wu <yingjun.wu@ibm.com>
2017-10-26 09:05:52 -0700

    add more functionalities

--- src/client_main.cpp
+++ src/client_main.cpp
@@ -9,7 +9,7 @@
 
 FastRandom fast_rand;
 
-size_t table_size = 100;
+size_t table_size = 1000;
 
 void Populate(pqxx::work &txn_handle, const size_t table_size) {
   for (size_t i = 0; i < table_size; ++i) {
@@ -17,18 +17,59 @@ void Populate(pqxx::work &txn_handle, const size_t table_size) {
   }
 }
 
-
-void Select(pqxx::work &txn_handle, const size_t num_tuple) {
-  for (size_t i = 0; i < num_tuple; ++i) {
-    pqxx::result R = txn_handle.exec("SELECT name FROM employee WHERE id=" + std::to_string(fast_rand.next() % table_size) + ";");
-    printf("txn result set size = %lu\n", R.size());
+void Select(pqxx::connection &conn, pqxx::work &txn_handle, const size_t num_tuple, bool with_prepare) {
+  if (with_prepare == true) {
+    conn.prepare("read", "SELECT name FROM employee WHERE id=$1");
+    for (size_t i = 0; i < num_tuple; ++i) {
+      pqxx::result R = txn_handle.prepared("read")(fast_rand.next() % table_size).exec();
+      printf("txn result set size = %lu\n", R.size());
+    }
+  } else {
+    for (size_t i = 0; i < num_tuple; ++i) {
+      pqxx::result R = txn_handle.exec("SELECT name FROM employee WHERE id=" + std::to_string(fast_rand.next() % table_size) + ";");
+      printf("txn result set size = %lu\n", R.size());
+    }
   }
 }
 
+void Update(pqxx::connection &conn, pqxx::work &txn_handle, const size_t num_tuple, bool with_prepare) {
+  if (with_prepare == true) {
+    conn.prepare("write", "UPDATE employee SET name = 'z' WHERE id=$1");
+    for (size_t i = 0; i < num_tuple; ++i) {
+      txn_handle.prepared("write")(fast_rand.next() % table_size).exec();
+    }
+  } else {
+    for (size_t i = 0; i < num_tuple; ++i) {
+      txn_handle.exec("UPDATE employee SET name = 'z' WHERE id=" + std::to_string(fast_rand.next() % table_size) + ";");
+    }
+  }
+}
 
-void Update(pqxx::work &txn_handle, const size_t num_tuple) {
-  for (size_t i = 0; i < num_tuple; ++i) {
-    txn_handle.exec("UPDATE employee SET name = 'z' WHERE id=" + std::to_string(fast_rand.next() % table_size) + ";");
+void Mix(pqxx::connection &conn, pqxx::work &txn_handle, const size_t num_tuple, const float read_ratio, bool with_prepare) {
+  if (with_prepare == true) {
+    conn.prepare("read", "SELECT name FROM employee WHERE id=$1");
+    conn.prepare("write", "UPDATE employee SET name = 'z' WHERE id=$1");
+    for (size_t i = 0; i < num_tuple; ++i) {
+      if (fast_rand.next_uniform() < read_ratio) {
+        // select
+        pqxx::result R = txn_handle.prepared("read")(fast_rand.next() % table_size).exec();
+        printf("txn result set size = %lu\n", R.size());
+      } else {
+        // update
+        txn_handle.prepared("write")(fast_rand.next() % table_size).exec();
+      }
+    }
+  } else {
+    for (size_t i = 0; i < num_tuple; ++i) {
+      if (fast_rand.next_uniform() < read_ratio) {
+        // select
+        pqxx::result R = txn_handle.exec("SELECT name FROM employee WHERE id=" + std::to_string(fast_rand.next() % table_size) + ";");
+        printf("txn result set size = %lu\n", R.size());      
+      } else {
+        // update
+        txn_handle.exec("UPDATE employee SET name = 'z' WHERE id=" + std::to_string(fast_rand.next() % table_size) + ";");
+      }
+    }
   }
 }
 
@@ -44,16 +85,18 @@ void Scan(pqxx::work &txn_handle) {
 
 int main(int argc, char **argv) {
 
-  if (argc != 2 && argc != 3 && argc != 4) {
+  if (argc != 2 && argc != 4 && argc != 5 && argc != 6) {
     printf("Usage: %s populate table_size with_index\n", argv[0]);
-    printf("Usage: %s select table_size num_tuple\n", argv[0]);
-    printf("Usage: %s update table_size num_tuple\n", argv[0]);
+    printf("Usage: %s select table_size num_tuple with_prepare\n", argv[0]);
+    printf("Usage: %s update table_size num_tuple with_prepare\n", argv[0]);
+    printf("Usage: %s mix table_size num_tuple read_ratio with_prepare\n", argv[0]);
+    printf("Usage: %s scan\n", argv[0]);
     return -1;
   }
 
   try {
     pqxx::connection C(
-      "host=172.19.146.5 port=5432 user=postgres sslmode=disable"
+      "host=dev2.db.pdl.cmu.local port=5432 user=postgres sslmode=disable"
     );
     printf("Connected to %s\n", C.dbname());
 
@@ -78,31 +121,47 @@ int main(int argc, char **argv) {
 
     } else if (std::string(argv[1]) == "select") {
 
-      assert(argc == 4);
+      assert(argc == 5);
 
       table_size  = atoi(argv[2]);
       size_t num_tuple = atoi(argv[3]);
-      
-      printf("select from table!\n");
+      bool with_prepare = bool(atoi(argv[4]));
+
+      printf("=========SELECT==========\n");
       pqxx::work txn(C);
-      Select(txn, num_tuple);
+      Select(C, txn, num_tuple, with_prepare);
       txn.commit();
     
     } else if (std::string(argv[1]) == "update") {
 
-      assert(argc == 4);
+      assert(argc == 5);
 
       table_size  = atoi(argv[2]);
       size_t num_tuple = atoi(argv[3]);
+      bool with_prepare = bool(atoi(argv[4]));
 
-      printf("update table!\n");
+      printf("=========UPDATE==========\n");
       pqxx::work txn(C);
-      Update(txn, num_tuple);
+      Update(C, txn, num_tuple, with_prepare);
+      txn.commit();
+
+    } else if (std::string(argv[1]) == "mix") {
+
+      assert(argc == 6);
+
+      table_size = atoi(argv[2]);
+      size_t num_tuple = atoi(argv[3]);
+      float read_ratio = atof(argv[4]);
+      bool with_prepare = bool(atoi(argv[4]));
+
+      printf("=========MIX==========\n");
+      pqxx::work txn(C);
+      Mix(C, txn, num_tuple, read_ratio, with_prepare);
       txn.commit();
 
     } else if (std::string(argv[1]) == "scan") {
 
-      printf("scan table!\n");
+      printf("=========SCAN==========\n");
       pqxx::work txn(C);
       Scan(txn);
       txn.commit();
